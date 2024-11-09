ae573a3363f27a123b5c40b5b11de93e9f3d9be2
Prajyot Patil <prajyotbpatil2744@gmail.com>
2024-11-09 22:17:47 +0530

    routing done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/About'
+import Appoinment from './pages/Appoinment'
+import Contact from './pages/Contact'
+import Doctors from './pages/doctors'
+import Login from './pages/Login'
+import Myappointment from './pages/Myappointment'
+import Myprofile from './pages/Myprofile'
 
 const App = () => {
   return (
-    <div className='bg-red-400'>
-      testing
+    <div className='mx-4 sm-mx:[10%]'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/About' element={<About/>}/>
+        <Route path='/appointment/:docid' element={<Appoinment/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/doctors' element={<Doctors/>}/>
+        <Route path='/doctors/:speciality' element={<Doctors/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/myappointment' element={<Myappointment/>}/>
+        <Route path='myprofile' element={<Myprofile/>}/>
+        
+      </Routes>
+      
     </div>
   )
 }
---
+++ frontend/src/pages/Appoinment.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Appoinment = () => {
+  return (
+    <div>
+      Appointmrnt
+    </div>
+  )
+}
+
+export default Appoinment
