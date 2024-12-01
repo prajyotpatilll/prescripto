0dfbd409261629fdb320e6aca4a3d62398baaa69
Prajyot Patil <prajyotbpatil2744@gmail.com>
2024-12-01 22:24:49 +0530

    controller and multer

---
+++ backend/controllers/doctorcontroller.js
@@ -0,0 +1,12 @@
+
+
+
+
+const adddoctor = async (req, res)=>{
+    try {
+        const {name , email, password, speciality, degree, exprience, about, available, fees, address, date, slotbooked} = req.body
+    } catch (error) {
+        
+    }
+
+}
